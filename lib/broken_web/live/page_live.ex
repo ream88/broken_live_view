a2e81e8904afc3d58d0142bdbeb9987f60076951
Mario Uher <uher.mario@gmail.com>
2019-04-28 15:13:20 +0200

    Implement example

--- lib/broken_web/live/page_live.ex
+++ lib/broken_web/live/page_live.ex
@@ -6,6 +6,31 @@ defmodule BrokenWeb.PageLive do
   end
 
   def mount(_session, socket) do
+    socket =
+      socket
+      |> assign(texts: ["foo", "bar"])
+
     {:ok, socket}
+  end
+
+  def handle_event("update", %{"id" => id, "text" => new_text}, socket) do
+    id = String.to_integer(id)
+
+    texts =
+      socket.assigns.texts
+      |> Enum.with_index()
+      |> Enum.map(fn {text, index} ->
+        case index do
+          ^id -> new_text
+          _ -> text
+        end
+      end)
+      |> IO.inspect()
+
+    socket =
+      socket
+      |> assign(texts: texts)
+
+    {:noreply, socket}
   end
 end
